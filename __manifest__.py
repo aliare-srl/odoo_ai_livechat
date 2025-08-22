56e9b14d20cbe4c6a9577144cf22bccb3ec2e833
sawandrew93 <sawandrew93@gmail.com>
2025-08-22 14:29:52 +0630

    fix3

--- __manifest__.py
+++ __manifest__.py
@@ -5,15 +5,7 @@
     'summary': 'AI Agent integration with Odoo Livechat and agent handoff',
     'license': 'LGPL-3',
     'depends': ['im_livechat', 'mail'],
-    'data': [
-        'views/livechat_channel_views.xml',
-        'views/res_config_settings_views.xml',
-    ],
-    'assets': {
-        'web.assets_frontend': [
-            'ai_livechat/static/src/js/ai_livechat.js',
-        ],
-    },
+    'data': [],
     'installable': True,
     'auto_install': False,
     'application': False,
