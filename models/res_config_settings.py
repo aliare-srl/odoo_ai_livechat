57dd65ec1d0943aeaee36eeb20e1fde89353428a
Aliaresrl <143626551+Aliaresrl@users.noreply.github.com>
2025-08-26 10:09:41 -0300

    Update res_config_settings.py

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -3,7 +3,9 @@ from odoo import models, fields
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    default_ai_endpoint = fields.Char('Default AI Endpoint', 
-                                     config_parameter='ai_livechat.default_endpoint')
+    ai_endpoint = fields.Char(
+        string="AI Endpoint",
+        config_parameter='mi_modulo.ai_endpoint'
+    )
     default_ai_api_key = fields.Char('Default AI API Key',
                                     config_parameter='ai_livechat.default_api_key')
